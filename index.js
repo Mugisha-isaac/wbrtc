7b017b3f1bac6903ead77f83277da09f51bdbc72
Mugisha-isaac <mugishaisaac2001@gmail.com>
2021-12-16 21:52:24 +0200

    done with setting up server inital configurations

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ const port = process.env.port || 8080;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.get('/join',(req,res)=>{
+    res.send({link: uuidV4()})
+})
+
+serve.listen(port,()=>{
+    console.log(`Listening on port ${port}`);
+}).on(error=>{
+    console.log('Error: ',error)
+})
